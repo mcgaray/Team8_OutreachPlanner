b97aafa79f5937de2c52473a0621a59f704ce2fa
SirChubbx17 <qjccdelacruz@gmail.com>
2022-10-14 23:41:53 +0800

    working dashboard and navbar

--- outreach_planner/planner/views.py
+++ outreach_planner/planner/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from .models import Event
+
 
 def home(request):
-    return render(request, 'dashboard.html', {})
+    event_list = Event.objects.all()
+    return render(request, 'dashboard.html', 
+    {'event_list': event_list})
