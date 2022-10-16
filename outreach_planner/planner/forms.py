db1e351b51e33c697b305b5d41a8f39c409745e1
eriesegui <qemcsegui@tip.edu.ph>
2022-10-16 18:23:29 +0800

    Added Venue Page

---
+++ outreach_planner/planner/forms.py
@@ -0,0 +1,25 @@
+from django import forms
+from django import forms
+from django.forms import EmailField, EmailInput, ModelForm
+from .models import Venue
+
+#Venue Form
+class VenueForm(ModelForm):
+    class Meta:
+        model = Venue
+        fields = "__all__"
+        labels = {
+            'venue_name': '',
+            'address': '',
+            'web_link': '',
+            'phone': '',
+            'email': '',
+        }
+        widgets = {
+            'venue_name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Venue Name'}),
+            'address': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Address'}),
+            'web_link': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Web Link'}),
+            'phone': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Phone'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Contact Email'}),
+
+        }
--- outreach_planner/planner/views.py
+++ outreach_planner/planner/views.py
@@ -1,8 +1,24 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 from .models import Event
+from .forms import VenueForm
 
 
 def home(request):
     event_list = Event.objects.all()
     return render(request, 'dashboard.html', 
     {'event_list': event_list})
+
+def add_venue(request):
+    submitted = False
+    if request.method == "POST":
+        form = VenueForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/add_venue?submitted=True')
+    else: 
+        form = VenueForm
+        if 'submitted' in request.GET:
+            submitted = True
+
+    return render(request, 'add_venue.html', {'form': form, 'submitted': submitted})
