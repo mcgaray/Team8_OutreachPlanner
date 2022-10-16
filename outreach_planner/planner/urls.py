c201555bc3cd36dfe5f8d5cedfe2d61cfc00ad19
eriesegui <qemcsegui@tip.edu.ph>
2022-10-16 19:41:35 +0800

    Add Show Venue and Venue List Page

--- outreach_planner/planner/urls.py
+++ outreach_planner/planner/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('add_venue', views.add_venue, name='add-venue'),
+    path('list_venue', views.list_venue, name='list-venue'),
+    path('show_venue/<venue_id>', views.show_venue, name='show-venue'),
 ]
