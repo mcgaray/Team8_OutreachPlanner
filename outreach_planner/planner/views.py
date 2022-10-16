133e148aaad43b30af45617fb6f879225871eb13
eriesegui <qemcsegui@tip.edu.ph>
2022-10-16 18:49:47 +0800

    Cleaning Repo

--- outreach_planner/planner/views.py
+++ outreach_planner/planner/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 from .models import Event
 from .forms import VenueForm
